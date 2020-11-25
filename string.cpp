ddb2ef9bc3629823c8ff8595fabbd9eee9da82ad
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-11-26 00:37:43 +0530

    Range Length problem

--- string.cpp
+++ string.cpp
@@ -1,10 +1,97 @@
-#include<iostream>
+
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int main(){
-
-    string name;
-    getline(cin,name);
-    cout<<"Hello "<<name;
-    return 0;
+char *encode(char *src);    
+ 
+int main() {
+	
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		char str[10000];
+		cin>>str;
+		
+		cout<<encode(str)<<endl;
+	}
+	return 0;
 }
+
+
+/*You are required to complete this function */
+
+char *encode(char *src)
+{     
+  int rLen;
+  char count[100001];
+  int len = strlen(src);
+ 
+  char *dest =new char[len];
+  
+  int i, j = 0, k;
+ 
+ 
+  for(i = 0; i < len; i++)
+  {
+ 
+    
+    dest[j++] = src[i];
+     
+    
+    rLen = 1;     
+    while(i + 1 < len && src[i] == src[i+1])
+    {
+      rLen++;
+      i++;
+    }   
+     
+    
+    sprintf(count, "%d", rLen);
+
+
+    for(k = 0; *(count+k); k++, j++)
+    { 
+      dest[j] = count[k]; 
+    } 
+  }  
+ 
+
+  dest[j] = '\0';
+  return dest;
+}     
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
