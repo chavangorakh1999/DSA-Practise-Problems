063427f3e0d2f3aa89acce3ed5a4c04edfc9e5cf
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-11-29 14:03:27 +0530

    optimised solution for array manipulation.

---
+++ arrayManipulation.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    // T is number of test cases size is actual size of array.
+    int T,size;
+    cin>>size>>T;
+
+    //p is starting index q is ending index and num is entity by which index is to be incremented.
+    int p,q,num;
+    vector<long int> v(size+1,0);
+    // Taking input for T times and incrementing p index and decrementing q+1 index buy num. 
+    for(int i=0; i< T;i++){
+        cin>>p>>q>>num;
+        v[p] += num;
+        if(q+1<=size){
+            v[q+1]-=num;
+        }
+    }
+    // calculating the prefix sum.
+    for(int i= 1;i<v.size();i++){
+        v[i]+=v[i-1];
+    }
+   
+     for(int i= 1;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+
+   //Printing the max element 
+    cout<<"MAX:"<<*max_element(v.begin(), v.end())<<endl;
+   
+    return 0;
+}
+
+
+// Fastest solution you can think of.
+// time complexity is O(2n) i.e O(n) and space complexity is O(n).
