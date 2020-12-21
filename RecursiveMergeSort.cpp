176499ce4c1d9acad73846deca26696ca7763653
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-12-21 19:43:38 +0530

    adding recursionMerge Sort

---
+++ RecursiveMergeSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void mergeSort(int *a, int startIndex, int endIndex)
+{
+    if (startIndex > endIndex)
+    {
+        return;
+    }
+    int mid = (startIndex + endIndex) / 2;
+    mergeSort(a, startIndex, mid);
+    mergeSort(a, mid + 1, endIndex);
+    int b[endIndex+1];
+    int k = 0;
+    for (int i = startIndex, j = mid + 1; j <= endIndex; i++, j++)
+    {
+        if (a[i] > a[j])
+        {
+            b[k++] = a[j];
+        }
+        else
+        {
+            b[k++] = a[i];
+        }
+    }
+    for(int i=0;i<=endIndex;i++){
+        a[i]=b[i];
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    mergeSort(a,0,n-1);
+    return 0;
+}
---
+++ Test/Test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by Gorakh Chavan on 20/12/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
