c8042974a3b8a99f41e6d939f6d9858f2aa301c5
chavangorakh1999 <chavangorakh19999@gmail.com>
2021-01-19 00:39:56 +0530

    takeInput function for treeNode

--- Trees/treeOperations.cpp
+++ Trees/treeOperations.cpp
@@ -2,14 +2,29 @@
 #include "tree.h"
 using namespace std;
 
-TreeNode<int>* takeInput(TreeNode<int>* root){
-
- 
+TreeNode<int>* takeInput(){
+    int data;
+    cout<<"Enter data"<<endl;
+    cin>>data;
+    TreeNode<int>* root= new TreeNode<int>(data);
+    int n;
+    cout<<"Enter number of children for "<<data;
+    for(int i=0;i<n;i++){
+        TreeNode<int>* child=takeInput();
+        root->children.push_back(child);
+    }
 return root;
 }
 
 void printTree(TreeNode<int>* root){
-    cout<<root->data<<endl;
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<":";
+      for(int i=0;i<root->children.size();i++){
+        cout<<root->children[i]->data<<",";
+    }
+    cout<<endl;
     for(int i=0;i<root->children.size();i++){
         printTree(root->children[i]);
     }
