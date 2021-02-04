a22a002d93a445b060e9d488f1ed41f560636118
chavangorakh1999 <chavangorakh19999@gmail.com>
2021-02-04 11:24:34 +0530

    tracking path fron specific node to root

--- BST/createBstFromSortedArray.cpp
+++ BST/createBstFromSortedArray.cpp
@@ -36,11 +36,41 @@ void printTree(BinarySearchTreeNode<int>* root){
     }
 }
 
+vector<int>* rootToNodePath(BinarySearchTreeNode<int>* root,int data){
+
+    if(root== NULL){
+        return NULL;
+    }
+    if(root->data == data){
+        vector<int>* output= new vector<int>();
+        output->push_back(root->data);
+        return output;
+    }
+    vector<int> * leftOutput= rootToNodePath(root->left,data);
+    if(leftOutput!=NULL){
+        leftOutput->push_back(root->data);
+        return leftOutput;
+    }
+    vector<int>* rightOutput= rootToNodePath(root->right,data);
+    if(rightOutput!=NULL){
+        rightOutput->push_back(root->data);
+        return rightOutput;
+    }else{
+        return NULL;
+    }
+
+
+}
+
 int main()
 {
     int arr[7]={1,2,3,4,5,6,7};
 
     BinarySearchTreeNode<int>* root=arrayInput(arr,0,6);
     printTree(root);
+    vector<int>* result=rootToNodePath(root,7);
+    for(auto x : *result){
+        cout<<x<<" ";
+    }
     return 0;
 }
