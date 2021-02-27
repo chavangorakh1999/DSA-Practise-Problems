1b944b2525a072500ecb23df714bf8665d9fca4d
chavangorakh1999 <chavangorakh19999@gmail.com>
2021-02-27 20:08:21 +0530

    removing duplicates

--- hashMap/removeDuplicates.cpp
+++ hashMap/removeDuplicates.cpp
@@ -1,12 +1,32 @@
 #include<iostream>
 #include<unordered_map>
+#include<vector>
 using namespace std;
+
+vector<int> removeDuplicate(int* arr,int n){
+    vector<int> result;
+    unordered_map<int,bool> seen;
+    for(int i=0;i<n;i++){
+        if(seen.count(arr[i])>0){
+            continue;
+        }
+        seen[arr[i]]=true;
+        result.push_back(arr[i]);
+    }
+    return result;
+}
 
 int main(){
 
     int n;
     cin>>n;
-        
-    unordered_map<int,bool> res;
+    int arr[n];
+     for(int i=0;i<n;i++){
+         cin>>arr[i];
+     }
+     vector<int>result=removeDuplicate(arr,n);
+     for(auto x: result){
+         cout<<x<<" ";
+     }
     return 0;
 }
