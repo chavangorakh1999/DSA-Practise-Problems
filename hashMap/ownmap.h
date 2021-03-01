ee3cd7cacd26d49379d4c71794c8e9cdf2207937
chavangorakh1999 <chavangorakh19999@gmail.com>
2021-03-02 00:06:28 +0530

    insert key value pair inside map

--- hashMap/ownmap.h
+++ hashMap/ownmap.h
@@ -30,6 +30,56 @@ class ourmap{
         count=0;
         numBuckets=5;
         buckets = new MapNode<V>*[numBuckets]; 
+        for(int i=0;i<numBuckets;i++){
+            buckets[i]=NULL;
+        }
+    }
+    ~ourmap(){
+         for(int i=0;i<numBuckets;i++){
+            delete buckets[i];
+        }
+        delete [] buckets;
+    }
+    int size() { 
+        return count;
     }
 
-}
+    V getValue(string key){
+
+    }
+    private:
+    int getBucketIndex(string key){
+        int hashCode=0;
+        int currentCoeff=1;
+        for(int i=0;i<key.length();i++){
+            hashCode+=key[i]*currentCoeff;
+            hashCode = hashCode % numBuckets;
+            currentCoeff *=37;
+            currentCoeff =currentCoeff% numBuckets;
+        }
+        return hashCode % numBuckets;
+    }
+
+    public:
+    void insert(string key,V value){
+        int bucketIndex=getBucketIndex(key);
+        MapNode<V>* head=new MapNode<V>(key,value);
+        while(head!=NULL){
+            if(head->key==key){
+                head->value=value;
+                return;
+            }
+            head=head->next;
+        }
+        MapNode<V>* newNode=new MapNode<V>(key,value);
+        newNode->next=head;
+        buckets[bucketIndex]=newNode;
+        count++;
+    }
+    V remove(string key){
+
+    }
+
+    
+
+};
