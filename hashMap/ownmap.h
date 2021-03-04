f8d16c4eae2ef101586638bca79d1f120d6101cf
chavangorakh1999 <chavangorakh19999@gmail.com>
2021-03-04 12:04:46 +0530

    implementing rehash in map

--- hashMap/ownmap.h
+++ hashMap/ownmap.h
@@ -60,6 +60,30 @@ class ourmap{
         return hashCode % numBuckets;
     }
 
+    void rehash(){
+        MapNode<V>** temp=buckets;
+        buckets= new MapNode<V>*[2*numBuckets];
+        for(int i=0;i<2*numBuckets;i++){
+            buckets[i]=NULL;
+        }
+        int oldNumBuckets=numBuckets;
+        numBuckets *=2;
+        int count=0;
+        for(int i=0;i< oldNumBuckets;i++){
+             MapNode<V>* head=temp[i];
+            while(head!=NULL){
+                string key=head->key;
+                V value=head->value;
+                insert(key,value);
+                head=head->next;
+            }
+        }
+        for(int i=0;i<oldNumBuckets;i++){
+            MapNode<V>* head=temp[i];
+            delete head;
+        }
+        delete [] temp;
+    }
     public:
     void insert(string key,V value){
         int bucketIndex=getBucketIndex(key);
@@ -75,6 +99,10 @@ class ourmap{
         newNode->next=head;
         buckets[bucketIndex]=newNode;
         count++;
+        double loadfactors=(1.0*count)/numBuckets;
+    if(loadfactors > 0.7){
+        rehash();
+    }
     }
     V remove(string key){
         int bucketIndex=getBucketIndex(key);
