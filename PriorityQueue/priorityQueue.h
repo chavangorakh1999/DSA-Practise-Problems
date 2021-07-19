e15a285b0ad11112a3acf8a2c37d45ce6f9ef557
chavangorakh1999 <chavangorakh19999@gmail.com>
2021-07-19 14:50:20 +0530

    starting preperation

--- PriorityQueue/priorityQueue.h
+++ PriorityQueue/priorityQueue.h
@@ -50,19 +50,47 @@ public:
             childIndex = parentIndex;
         }
     }
-    void display(){
-        for(int i=0;i<pq.size();i++){
-            cout<<pq[i]<<" ";
+    void display()
+    {
+        for (int i = 0; i < pq.size(); i++)
+        {
+            cout << pq[i] << " ";
         }
     }
-    int removeMin(){
-        int ans=pq[0];
-        pq[0]=pq[pq.size()-1];
+    int removeMin()
+    {
+        if (isEmpty())
+        {
+            return 0;
+        }
+        int ans = pq[0];
+        pq[0] = pq[pq.size() - 1];
         pq.pop_back();
-        
-        int minIndex,leftChildIndex,rightChildIndex,parentIndex=0;
-        leftChildIndex=parentIndex+1;
-        rightChildIndex=parentIndex+2;
+
+        int minIndex, leftChildIndex, rightChildIndex, parentIndex = 0;
+        parentIndex = 0;
+        leftChildIndex = (2 * parentIndex) + 1;
+        rightChildIndex = (2 * parentIndex) + 2;
+
+        while (leftChildIndex < pq.size())
+        {
+            minIndex = parentIndex;
+            if (pq[leftChildIndex] < pq[minIndex])
+            {
+                minIndex = leftChildIndex;
+            }
+            if (rightChildIndex < pq.size() && pq[rightChildIndex] < pq[minIndex])
+            {
+                minIndex = rightChildIndex;
+            }
+            int temp = pq[minIndex];
+            pq[minIndex] = pq[parentIndex];
+            pq[parentIndex] = temp;
+
+            parentIndex = minIndex;
+            leftChildIndex = (2 * parentIndex) + 1;
+            rightChildIndex = (2 * parentIndex) + 2;
+        }
 
         return ans;
     }
